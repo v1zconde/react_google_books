9d76b61d824c88a6223da9c565211162e5a17c3b
rvasquez <rvasquez1@gmail.com>
2021-03-11 17:43:59 -0500

    build folder

--- client/src/index.js
+++ client/src/index.js
@@ -6,4 +6,5 @@ import registerServiceWorker from "./registerServiceWorker";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
 registerServiceWorker();
